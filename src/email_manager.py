d4306801845626490ae4bd781af2f770e3c9aff8
voynow <voynow99@gmail.com>
2024-08-05 19:27:33 -0400

    cleanup html gen

--- src/email_manager.py
+++ src/email_manager.py
@@ -1,18 +1,52 @@
 import os
+import uuid
 from typing import Dict
 
 import sib_api_v3_sdk
 from dotenv import load_dotenv
+
+from src import constants
+from src.types.training_week import TrainingWeek
 
 load_dotenv()
 
 configuration = sib_api_v3_sdk.Configuration()
 configuration.api_key["api-key"] = os.environ["EMAIL_API_KEY"]
 
-
 api_instance = sib_api_v3_sdk.TransactionalEmailsApi(
     sib_api_v3_sdk.ApiClient(configuration)
 )
+
+
+def training_week_to_html(training_week: TrainingWeek) -> str:
+    """
+    Convert a TrainingWeek object to HTML content for email
+
+    :param training_week: TrainingWeek object
+    :return: HTML content for email
+    """
+    uid = str(uuid.uuid4())
+    html_content = constants.training_week_html_base
+    for day, session in training_week.dict().items():
+        html_content += f"""
+                    <li>
+                        <strong>{day.capitalize()}</strong>
+                        <span>{session['session_type'].value} {session['distance']} miles</span><br>
+                        <span>Notes: {session['notes']}</span>
+                    </li>
+        """
+    html_content += f"""
+                </ul>
+            </div>
+            <div class="footer">
+                <p style="font-size: 15px; color: #777;">Powered by the Strava API and OpenAI</p>
+                <p>{uid}</p>
+            </div>
+        </div>
+    </body>
+    </html>
+    """
+    return html_content
 
 
 def send_email(
