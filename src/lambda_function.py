865fc57df046c3774d68f3edba35ed1b4ce07c82
voynow <voynow99@gmail.com>
2024-08-06 20:03:54 -0400

    implementing lambda functionality

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,2 +1,28 @@
+import os
+
+from src.activities import (
+    get_activities_df,
+    get_day_of_week_summaries,
+    get_weekly_summaries,
+)
+from src.auth_manager import get_strava_client
+from src.email_manager import send_email, training_week_to_html
+from src.training_week import generate_training_week
+
+
 def lambda_handler(event, context):
-    return {"statusCode": 200, "body": "Hello, world!"}
+
+    athlete_id = os.environ["JAMIES_ATHLETE_ID"]
+    strava_client = get_strava_client(athlete_id)
+
+    activities_df = get_activities_df(strava_client)
+    day_of_week_summaries = get_day_of_week_summaries(activities_df)
+    weekly_summaries = get_weekly_summaries(activities_df)
+
+    training_week = generate_training_week(weekly_summaries, day_of_week_summaries)
+
+    subject = "Training Schedule Just Dropped 🏃"
+    send_email(
+        subject=subject,
+        html_content=training_week_to_html(training_week),
+    )
