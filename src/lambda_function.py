ca44b96764bb69c90b97fabda3749f4aeda998d6
voynow <voynow99@gmail.com>
2024-08-10 12:14:13 -0400

    training week with coaching table in supabase

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -7,6 +7,7 @@ from src.activities import (
 )
 from src.auth_manager import get_strava_client
 from src.email_manager import send_email, training_week_to_html
+from src.supabase_client import upsert_training_week_with_coaching
 from src.training_week import generate_training_week
 
 
@@ -15,14 +16,18 @@ def lambda_handler(event, context):
     athlete_id = os.environ["JAMIES_ATHLETE_ID"]
     strava_client = get_strava_client(athlete_id)
 
+    # process activities data
     activities_df = get_activities_df(strava_client)
     day_of_week_summaries = get_day_of_week_summaries(activities_df)
     weekly_summaries = get_weekly_summaries(activities_df)
 
+    # Generate the training week
     training_week = generate_training_week(weekly_summaries, day_of_week_summaries)
+    upsert_training_week_with_coaching(
+        athlete_id=athlete_id, training_week_with_coaching=training_week
+    )
 
-    subject = "Training Schedule Just Dropped 🏃"
     send_email(
-        subject=subject,
+        subject="Training Schedule Just Dropped 🏃",
         html_content=training_week_to_html(training_week),
     )
--- src/supabase_client.py
+++ src/supabase_client.py
@@ -1,11 +1,11 @@
 import datetime
 import os
-from typing import Dict
 
 from dotenv import load_dotenv
 from postgrest.base_request_builder import APIResponse
 from supabase import Client, create_client
 
+from src.types.training_week_with_coaching import TrainingWeekWithCoaching
 from src.types.user_auth_row import UserAuthRow
 
 load_dotenv()
@@ -54,3 +54,27 @@ def get_user_auth(athlete_id: int) -> UserAuthRow:
         raise ValueError(f"Cound not find user_auth row with {athlete_id=}")
 
     return UserAuthRow(**response.data[0])
+
+
+def upsert_training_week_with_coaching(
+    athlete_id: int,
+    training_week_with_coaching: TrainingWeekWithCoaching,
+) -> APIResponse:
+    """
+    Upsert a row into the training_week_with_coaching table.
+
+    :param athlete_id: The ID of the athlete.
+    :param training_week_with_coaching: An instance of TrainingWeekWithCoaching.
+    :return: APIResponse from Supabase.
+    """
+    row_data = {
+        "athlete_id": athlete_id,
+        "training_week": training_week_with_coaching.training_week.json(),
+        "typical_week_training_review": training_week_with_coaching.typical_week_training_review,
+        "weekly_mileage_target": training_week_with_coaching.weekly_mileage_target,
+    }
+
+    table = client.table("training_week_with_coaching")
+    response = table.upsert(row_data).execute()
+
+    return response
