0c2276824eb7e78b59d6188a8fed800d76bc3687
voynow <voynow99@gmail.com>
2024-08-12 18:04:09 -0400

    bug fix

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -12,21 +12,26 @@ from src.training_week import generate_training_week
 
 
 def lambda_handler(event, context):
+    client_preferences = "A) Training for a marathon B) This will be my second marathon C) Prefer workouts on Wednesdays and long runs on Saturdays"
 
+    # activities setup
     athlete_id = os.environ["JAMIES_ATHLETE_ID"]
     strava_client = get_strava_client(athlete_id)
-
-    # process activities data
     activities_df = get_activities_df(strava_client)
+
+    # gen training week pipeline
     day_of_week_summaries = get_day_of_week_summaries(activities_df)
     weekly_summaries = get_weekly_summaries(activities_df)
+    training_week = generate_training_week(
+        client_preferences=client_preferences,
+        weekly_summaries=weekly_summaries,
+        day_of_week_summaries=day_of_week_summaries,
+    )
 
-    # Generate the training week
-    training_week = generate_training_week(weekly_summaries, day_of_week_summaries)
+    # save data to db and trigger email
     upsert_training_week_with_coaching(
         athlete_id=athlete_id, training_week_with_coaching=training_week
     )
-
     send_email(
         subject="Training Schedule Just Dropped 🏃",
         html_content=training_week_to_html(training_week),
