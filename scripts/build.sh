e90ad7f0aa242f93c36a3896522e953e87733a68
voynow <voynow99@gmail.com>
2024-08-13 19:25:21 -0400

    tested locally fingers crossed

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# Clean up any previous builds
 rm -rf package
 rm -f function.zip
 
+# Export dependencies and install them into the package directory
 poetry export -f requirements.txt --output requirements.txt --without-hashes
 pip install --target ./package -r requirements.txt
 
+# Navigate to the package directory and zip its contents
 cd package || exit
 zip -r ../function.zip .
 cd ..
 
-zip -r function.zip src
+# Zip the contents of the src directory directly into the function.zip
+cd src || exit
+zip -r ../function.zip .  # Note the dot here to include the contents, not the directory itself
+cd ..
