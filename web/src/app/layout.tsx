aca25fc20d86822f47833fe21fc7f2e681fa67f6
voynow <voynow99@gmail.com>
2024-09-12 20:39:30 -0400

    icon, nav, get started

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" sizes="any" />
+      </head>
       <body className={`${inter.className} bg-gray-100`}>
         {children}
       </body>
