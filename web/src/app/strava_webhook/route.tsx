40d6383f25236111c5046c449e2ec1bf6372211f
voynow <voynow99@gmail.com>
2024-10-06 11:01:36 -0400

    attempting fix for webhook route

--- web/src/app/strava_webhook/route.tsx
+++ web/src/app/strava_webhook/route.tsx
@@ -1,22 +1,22 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(req: NextRequest) {
-    const searchParams = req.nextUrl.searchParams;
+export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams;
     const mode = searchParams.get('hub.mode');
     const token = searchParams.get('hub.verify_token');
     const challenge = searchParams.get('hub.challenge');
 
-    if (mode === 'subscribe') {
+    if (mode === 'subscribe' && token === process.env.STRAVA_VERIFY_TOKEN) {
         return NextResponse.json({ 'hub.challenge': challenge });
     } else {
-        return NextResponse.json({ error: 'Invalid verify token' }, { status: 403 });
+        return new NextResponse('Invalid verify token', { status: 403 });
     }
 }
 
-export async function POST(req: NextRequest) {
-    const event = await req.json();
+export async function POST(request: NextRequest) {
+    const event = await request.json();
     try {
-        const response = await fetch('https://lwg77yq7dd.execute-api.us-east-1.amazonaws.com/prod/webhook', {
+        const response = await fetch('https://lwg77yq7dd.execute-api.us-east-1.amazonaws.com/prod/signup', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(event),
