1b8fb40165b9d8a9c5fbde76cb05f407d84cfaa0
voynow <voynow99@gmail.com>
2024-10-16 17:04:07 -0400

    Theme update

--- mobile/mobile/ColorTheme.swift
+++ mobile/mobile/ColorTheme.swift
@@ -1,16 +1,19 @@
 import SwiftUI
 
 struct ColorTheme {
-  static let white = Color(white: 0.975)
-  static let lightLightGrey = Color(white: 0.95)
-  static let lightGrey = Color(white: 0.925)
-  static let darkGrey = Color(white: 0.2)
-  static let darkDarkGrey = Color(white: 0.15)
-  static let superDarkGrey = Color(white: 0.1)
+  static let white = Color(white: 0.95)
+  static let lightGrey = Color(white: 0.9)
+  static let midLightGrey = Color(white: 0.8)
+  static let midDarkGrey = Color(white: 0.25)
+  static let darkGrey = Color(white: 0.166)
+  static let darkDarkGrey = Color(white: 0.133)
+  static let black = Color(white: 0.1)
 
   static let primary: Color = Color(red: 0.28, green: 0.5, blue: 1.0)  // #477eff
   static let primaryLight: Color = Color(red: 0.55, green: 0.68, blue: 1.0)  // #8caeff
   static let indigo: Color = Color(red: 0.42, green: 0.28, blue: 1.0)  // #6c47ff
-  static let green: Color = Color(red: 0.28, green: 1.0, blue: 0.42)  // #47ff6c
   static let redPink: Color = Color(red: 1.0, green: 0.278, blue: 0.494)  // #ff477e
+  static let orange: Color = Color(red: 1.0, green: 0.424, blue: 0.278)  // #ff6c47
+  static let yellow: Color = Color(red: 1.0, green: 0.784, blue: 0.278)  // #ffc847
+  static let green: Color = Color(red: 0.28, green: 1.0, blue: 0.42)  // #47ff6c
 }
