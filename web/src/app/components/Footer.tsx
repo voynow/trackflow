79ae802da846f8a3caa57943fb640549c9a4b673
voynow <voynow99@gmail.com>
2024-10-19 18:00:04 -0400

    new landing page !!!!!

--- web/src/app/components/Footer.tsx
+++ web/src/app/components/Footer.tsx
@@ -2,10 +2,10 @@ import Image from 'next/image';
 
 const Footer = (): JSX.Element => {
     return (
-        <footer className="mt-32 w-full bg-gray-100 text-gray-400 py-4">
+        <footer className="mt-32 w-full bg-gray-900 text-gray-320 py-4">
             <div className="container flex items-center justify-between">
                 <Image
-                    src="/powered_by_strava.png"
+                    src="/powered_by_strava_light.png"
                     alt="Powered by Strava"
                     width={162}
                     height={30}
