6ea88f4c6711c1951fadc08413ff8b08790b63db
voynow <voynow99@gmail.com>
2025-01-03 16:36:29 -0500

    new web app minor updates/finishing touches

--- webapp/src/app/components/Navbar.tsx
+++ webapp/src/app/components/Navbar.tsx
@@ -10,7 +10,7 @@ export default function Navbar(): React.ReactElement {
     return (
         <nav className="fixed top-0 w-full text-gray-100 z-10 bg-gray-900/80 backdrop-blur-sm">
             <div className="px-4 sm:px-8 flex justify-between items-center h-16">
-                <Link href="/" className={`text-2xl font-medium ${montserrat.className}`}>
+                <Link href="/" className={`text-xl font-bold ${montserrat.className}`}>
                     <span className="text-blue-300">Crush</span>{' '}
                     <span className="text-blue-500">Your Race</span>
                 </Link>
@@ -21,7 +21,7 @@ export default function Navbar(): React.ReactElement {
                     <svg className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                     </svg>
-                    <span>Dashboard</span>
+                    <span className="text-lg font-bold">Dashboard</span>
                 </button>
             </div>
         </nav>
--- webapp/src/app/dashboard/page.tsx
+++ webapp/src/app/dashboard/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import Link from 'next/link';
 import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
 
 const DashboardPage = () => {
     return (
         <div className="bg-gray-900 text-gray-100 min-h-screen">
+            <Navbar />
             <main className="container mx-auto px-6 py-24 sm:px-8 lg:px-12">
                 <motion.div
                     className="text-center mb-24"
@@ -25,12 +26,10 @@ const DashboardPage = () => {
                             In the meantime...
                         </h3>
                         <ul className="list-disc list-inside mb-8 text-left sm:text-lg md:text-xl">
-                            <li className="mb-2">Download our mobile app TrackFlowAI from <a href="https://apps.apple.com/us/app/trackflowai/id6737172627" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:text-blue-300 underline">the App Store</a>.</li>
-                            <li>Have questions? Reach out to us.</li>
+                            <li className="mb-2">Download our mobile app TrackFlowAI from <a href="https://apps.apple.com/us/app/trackflowai/id6737172627" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:text-blue-300 underline">the App Store</a></li>
+                            <li className="mb-2">Contact me at <a href="mailto:voynow99@gmail.com" className="text-blue-400 hover:text-blue-300 underline">voynow99@gmail.com</a> with any feedback</li>
+                            <li>And or connect with me on <a href="https://twitter.com/jamievoynow" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:text-blue-300 underline">X/Twitter</a></li>
                         </ul>
-                        <Link href="mailto:voynow99@gmail.com" className="px-8 py-4 text-xl text-gray-200 bg-blue-600 font-bold rounded-full hover:bg-blue-700 hover:scale-105 transition duration-300 ease-in-out shadow-lg hover:shadow-blue-500/50 inline-block">
-                            Contact @jamievoynow
-                        </Link>
                     </div>
                 </motion.div>
             </main>
